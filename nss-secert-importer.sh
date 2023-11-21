5dfd2ae01e1972d1a85bedd456a74e98307ff843
Yuval Kashtan <yuvalkashtan@gmail.com>
2023-11-21 04:13:23 +0200

    fixes

--- nss-secert-importer.sh
+++ nss-secert-importer.sh
@@ -1,6 +1,6 @@
-#!/bin/bash
+#!/bin/bash -xe
 
-NSS_DB_DIR="${NSS_DB:-/var/lib/ipsec/nss}"
+NSS_DB_DIR="${NSS_DB_DIR:-/var/lib/ipsec/nss}"
 echo "INFO: nss db dir=$NSS_DB_DIR"
 
 if [ ! -f $NSS_DB_DIR/cert9.db ]
@@ -8,6 +8,7 @@ then
 	certutil -N -d $NSS_DB_DIR --empty-password
 fi
 
-openssl pkcs12 -export -out tls.pfx -inkey /certs/tls.key -in /certs/tls.crt -certfile /certs/ca.crt
-pk12util -i tls.pfx -d . -W ""
+openssl pkcs12 -export -out tls.pfx -inkey /certs/tls.key -in /certs/tls.crt -certfile /certs/ca.crt -password env:TMP_PASS
+pk12util -i tls.pfx -d $NSS_DB_DIR -W "$TMP_PASS"
+rm tls.pfx
 certutil -A -n CA -i /certs/ca.crt -d $NSS_DB_DIR -t "CT,c,c"
